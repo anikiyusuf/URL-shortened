a8057b13ad065b43869df2ea2915c2b958fd5df4
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-06-30 21:30:32 +0100

    db file fix

--- db/db.js
+++ db/db.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL
 
 function  connectionMongoDB() {
-    const MONGO_DB_CONNECTION_URL = 'mongodb://localhost:27017'
-    mongoose.connect(MONGO_DB_CONNECTION_URL)};
-//, {erverSelectionTimeoutMS: 30000})
+    mongoose.connect(MONGO_DB_CONNECTION_URL , {
+        serverSelectionTimeoutMS: 30000})};
+
 
       
     mongoose.connection.on('connected', () =>{
@@ -26,30 +26,6 @@ function  connectionMongoDB() {
 
 
 
-
-
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// //const logger = require("./logger/logger")
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-// //console.log(MONGODB)
-// // connect to mongodb
-// function connectionMongoDB() {
-//     const rl = 'mongodb://localhost:27017'
-//     mongoose.connect(rl, {
-//     serverSelectionTimeoutMS: 30000});
-   
-//     mongoose.connection.on('connected', () => {
-//         console.log('Connection to mongodb successfully')
-//     })
-
-//     mongoose.connection.on('err', (err) => {
-//         console.error(err)
-//     })
-// }
-
-// module.exports =  {connectionMongoDB}
 
 
 
--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 require('dotenv').config()
 const express = require("express")
-const { connectionMongoDB } = require("./db/db");
+const  rateLimit = require('express-rate-limit')
+
+const { connectionMongoDB  } = require("./db/db")
+
 const app = express()
 const PORT = process.env.PORT
 
+
+
 connectionMongoDB()
+
+
 
 app.set('view engine', 'ejs')
 app.set('view engine')
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
 
-app.use("/", require("./routes/index"));
-app.use("/auth", require("./routes/userRoutes"));
-app.use("/", require("./routes/urlRoutes"));
-
-
-
-
-// app.get('/' , (req,res) => {
-//     res.render("index")
-// })
-
-// app.get("/url" , (req,res)=>{
-//     res.render('url')
-// })
-
-// app.get("/create" , (req,res) => {
-//     res.render('create')
-// })
-// app.get("/enter" , (req,res) => {
-//     res.render('enter')
-// })
+app.use("/" , require("./routes/index"))
+app.use("/auth" ,require("./routes/userRouter"))
+app.use("/" , require("./routes/urlRoutes"))
 
 
 
-app.listen(PORT , () => {
-    console.log(`server running localhost:${PORT}`)
+
+  
+// RATE liMITING FOR 30 minutes
+const limiter = rateLimit({
+	windowMs: 30 * 60 * 1000, // 30 minutes
+	max: 100, 
+	standardHeaders: true, 
+	legacyHeaders: false, 
 })
 
+app.use(limiter)
 
+app.use((err, req,res,  next) =>{
+    // logger.error(err.message)
+    console.log(err)
+    const errorStatus = err.status || 5000
+    res.status(errorStatus).send(err.message)
+    next()
+})
 
+app.listen(PORT , () => {
+console.log(`server running ${PORT} `)
+})
